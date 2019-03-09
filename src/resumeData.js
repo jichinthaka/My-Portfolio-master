33d4048308dab3bae0c63c191fad17237f97105c
jichinthaka <imesh.14@cse.mrt.ac.lk>
2019-03-09 14:38:51 +0530

    my first commit

---
+++ src/resumeData.js
@@ -0,0 +1,116 @@
+let resumeData = {
+    //"imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Imesh Chinthaka",
+    "currentRole": "Software Engineer at Sutra Technologies",
+    "otherTitles": "BSc Eng (Hons) University of Moratuwa | CIMA Cert BA",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/rbhatia46",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"http://twitter.com/rbhatia46",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am a dedicated, innovative and self-motivated person with the ability to adapt any Software & Hardware tools or working environment searching for Software Engineer position and interesting to work with new technologies in different fields. Have experience from designing, implementing, leading several successful intriguing projects. Efficient work style is guaranteed for individual project works as well as group projects.",
+    "address":"“Thilaka” , Samagi Mawatha, Duwa Temple Road, Kalutara South, Sri Lanka.",
+    "website":"https://notyet.com",
+    "keyCompetencies": [
+        {"competence": "Requirement Engineering"},
+        {"competence": "Object-Oriented Designing"},
+        {"competence": "UI / UX Implementation"},
+        {"competence": "Mathematical Analysis"},
+        {"competence": "Analytical Thinking"},
+        {"competence": "3D Geometry Modeling"},
+        {"competence": "Embedded Hardware Developing"},
+        {"competence": "Micro-Controller Programming"}
+    ],
+    "education":[
+      {
+        "UniversityName":"University of Moratuwa, Sri Lanka",
+        "specialization":"B.Sc. Engineering (Hons) in Computer Science and Engineering",
+        "YearRange":"2014 - 2018",
+        "Achievements":"GPA - 3.34 out of 4.2 (Second Class Upper Division)",
+          "Discription": "Faculty of Engineering in University of Moratuwa is the rank 1 Engineering Faculty in Sri Lanka, students are selected from highly competitive G.C.E.(A/L) examination"
+      },
+      {
+        "UniversityName":"Kalutara Vidyalaya National School, Sri Lanka",
+        "specialization":"GCE Advanced Level and GCE Ordinary Level",
+        "YearRange":"1999 - 2013",
+        "Achievements":"3 A’s for Combined Mathematics, Physics and Chemistry for GCE Advanced Level 2013 (Island rank: 263, District rank: 8), and 7 A’s 1 B and 1 C for GCE Ordinary Level"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
